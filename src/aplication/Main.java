9827cf3ffb9a186a4e5debf9e07b72a1f6e20d93
CarolinaCedro <yotechgirl@gmail.com>
2022-07-20 21:56:58 -0300

    feat: init main

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -1,7 +1,35 @@
 package aplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        int op;
+
+        do {
+            System.out.println("--- Star College ----");
+            System.out.println();
+            System.out.println("--- Menu ---");
+            System.out.println("(1)Professor");
+            System.out.println("(2)Estudante");
+            System.out.println("(3)Disciplinas");
+            System.out.println("(0)Sair");
+            op = sc.nextInt();
+        } while (op != 0);
+
+        switch (op) {
+            case 1: //chamar Professor
+                break;
+            case 2: //chamar Estudante
+                break;
+            case 3: //Chamar disciplinas
+                break;
+            default: //default
+        }
+
     }
 }
